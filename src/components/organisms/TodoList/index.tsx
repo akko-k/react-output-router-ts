9b9eecd9d3f235ad154e13fd8bd0e5b1c003ac2d
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-09-02 23:28:01 +0900

    organismsコンポーネントを追加

---
+++ src/components/organisms/TodoList/index.tsx
@@ -0,0 +1,69 @@
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { NAVIGATION_PATH } from '../../../constants/navigations';
+import {
+  faFile,
+  faPenToSquare,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { TodoType } from '../../../interfaces/Todo';
+import styles from './styles.module.css';
+
+type Props = {
+  filteredTodoList: Array<TodoType>;
+  deleteTodo: (id: number, title: string) => void;
+};
+/**
+ * Renders a list of todos.
+ *
+ * @component
+ * @param {Object} props
+ * @returns {JSX.Element}The TodoList component.
+ */
+export const TodoList: FC<Props> = ({
+  filteredTodoList,
+  deleteTodo,
+}) => {
+  const navigate = useNavigate();
+
+  const handleMoveEditPage = (id: number) => {
+    navigate(`${NAVIGATION_PATH.EDIT}/${id}`);
+  };
+
+  const handleMoveDetailPage = (id: number) => {
+    navigate(`${NAVIGATION_PATH.DETAIL}/${id}`);
+  };
+  return (
+    <>
+      <ul>
+        {filteredTodoList.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <div className={styles.icons}>
+              <FontAwesomeIcon
+                icon={faFile}
+                onClick={() =>
+                  handleMoveDetailPage(todo.id)
+                }
+                className={styles.icon}
+              />
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                onClick={() => handleMoveEditPage(todo.id)}
+                className={styles.icon}
+              />
+              <FontAwesomeIcon
+                icon={faTrashAlt}
+                onClick={() =>
+                  deleteTodo(todo.id, todo.title)
+                }
+                className={styles.icon}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
