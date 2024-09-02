5ce27dbaec114af1700476fa75ec247d49e4c7ab
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-09-02 23:32:00 +0900

    必要なファイルを調整

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routers';
+import { TodoProvider } from './contexts/TodoContext';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TodoProvider>
+      <RouterProvider router={router} />
+    </TodoProvider>
+  </React.StrictMode>,
+);
